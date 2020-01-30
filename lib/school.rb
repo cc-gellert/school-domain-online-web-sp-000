b92722bcd5d9d847ffaa0259a0f076a6d32cab89
Chloe Gell <gellert.chloe@gmail.com>
2020-01-30 18:45:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -14,6 +14,7 @@ class School
   
   def add_student(name, grade)
     ROSTER.each do |level, students|
+      if(level[grade] ||=[] )
       ROSTER[grade] ||= []
       ROSTER[grade] << name
     end 
