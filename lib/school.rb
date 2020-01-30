c5022d548c3351ec32c619a9d8cf7c6496f62d56
Chloe Gell <gellert.chloe@gmail.com>
2020-01-30 17:57:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,12 @@
 # code here!
 class School 
+  attr_accessor 
   attr_reader :name 
   def initialize(name)
     @name = name 
   end 
   ROSTER = {} 
+  
   def add_student(name, grade)
     ROSTER.each do |level, students|
       if(level == grade)
@@ -14,9 +16,11 @@ class School
     ROSTER[grade] = []
     ROSTER[grade] << name 
   end 
+  
   def grade(num)
     ROSTER[num]
   end 
+  
   def sort 
     ROSTER.sort 
   end 
