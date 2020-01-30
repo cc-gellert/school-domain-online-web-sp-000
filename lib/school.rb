0c886961e73165f5a32c4b43457d5875529ff641
Chloe Gell <gellert.chloe@gmail.com>
2020-01-30 17:48:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
 # code here!
+class School 
+  def initialize(name)
+    @name = name 
+  end 
+  roster = {} 
+  def add_student(name, grade)
+    roster.each |level, students|
+      if(level == grade)
+        students << name 
+      end 
+    end 
+    roster[grade] = []
+    roster[grade] << name 
+  end 
+  def grade(num)
+    roster[num]
+  end 
+  def sort 
+    roster.sort 
+  end 
+end 
